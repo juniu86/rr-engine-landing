1c66d57ccfc6ff84ec3f92e7341313cfd6324300
juniu86 <52000118+juniu86@users.noreply.github.com>
2026-05-04 19:00:45 -0300

    feat: configura dominio customizado engine.rres.com.br

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://juniu86.github.io',
-  base: '/rr-engine-landing/',
+  site: 'https://engine.rres.com.br',
 });
