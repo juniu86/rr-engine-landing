90cb87dde5f0b450721c27cd7294c2bb1be09970
juniu86 <52000118+juniu86@users.noreply.github.com>
2026-05-04 18:32:56 -0300

    feat: configura deploy automatico no GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://juniu86.github.io',
+  base: '/rr-engine-landing/',
+});
